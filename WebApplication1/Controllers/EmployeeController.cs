8e5b57273d3df19d25f99c93ed984582f12acf97
ruyanyun <ruyanyun@hotmail.com>
2018-11-20 23:30:33 +0800

    201811202330

--- WebApplication1/Controllers/EmployeeController.cs
+++ WebApplication1/Controllers/EmployeeController.cs
@@ -8,28 +8,45 @@ using WebApplication1.ViewModels;
 
 namespace WebApplication1.Controllers
 {
-    public class TestController : Controller
+    public class EmployeeController : Controller
     {
+        public ActionResult SaveEmployee (Employee e,string BtnSubmit)
+        {
+            switch (BtnSubmit)
+            {
+                case "Save Employee":
+                    return Content(e.FirstName + "|" + e.LastName + "|" + e.Salary);
+                case "Cancel":
+                    return RedirectToAction("Index");
+            }
+            return new EmptyResult();
+        }
         public string GetString()
         {
             return "Hello World is old now. It's time for wassup bro;";
         }
-
-        public ActionResult GetView()
+        public ActionResult AddNew()
         {
+            return View("CreateEmployee");
+        }
+
+        public ActionResult Index()
+
+        {
+
             EmployeeListViewModel employeeListViewModel = new EmployeeListViewModel();
 
             EmployeeBusinessLayer empBal = new EmployeeBusinessLayer();
             List<Employee> employees = empBal.GetEmployees();
 
             List<EmployeeViewModel> empViewModels = new List<EmployeeViewModel>();
-            
+
             foreach (Employee emp in employees)
             {
                 EmployeeViewModel empViewModel = new EmployeeViewModel();
                 empViewModel.EmployeeName = emp.FirstName + "" + emp.LastName;
                 empViewModel.Salary = emp.Salary.ToString("C");
-                if(emp.Salary>15000)
+                if (emp.Salary > 15000)
                 {
                     empViewModel.SalaryColor = "yellow";
 
@@ -37,18 +54,20 @@ namespace WebApplication1.Controllers
                 else
                 {
                     empViewModel.SalaryColor = "green";
-                 }
+                }
                 empViewModels.Add(empViewModel);
 
-                
+
             }
             employeeListViewModel.Employees = empViewModels;
-            employeeListViewModel.UserName = "Admin";
-            return View("Myview", employeeListViewModel);
-                
-         
-         
+            
+
+
+            return View("Index", employeeListViewModel);
+
+
+
         }
-      
+
     }
 }
