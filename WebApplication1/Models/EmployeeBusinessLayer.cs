8dbe9b1c0866cae7aadad9ac5dfe66f283fa4c01
ruyanyun <ruyanyun@hotmail.com>
2018-11-20 20:49:39 +0800

    初始化项目

---
+++ WebApplication1/Models/EmployeeBusinessLayer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class EmployeeBusinessLayer
+    {
+        public List<Employee> GetEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+            Employee emp = new Employee();
+            emp.FirstName = "andy";
+            emp.LastName = "qian";
+            emp.Salary = 14000;
+            employees.Add(emp);
+
+            emp = new Employee();
+            emp.FirstName = "qiumegn"
+;
+            emp.LastName = "wan";
+            emp.Salary = 40000;
+            employees.Add(emp);
+
+            emp = new Employee();
+            emp.FirstName = "tingting";
+            emp.LastName = "wan";
+            emp.Salary = 50000;
+            employees.Add(emp);
+
+
+
+
+
+                 return employees;
+        }
+    }
+}
