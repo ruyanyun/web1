0a86f149bcfd4b9131c87d9aa81ba42c1e2f81bb
ruyanyun <ruyanyun@hotmail.com>
2018-11-20 21:01:52 +0800

    第一次提交

--- WebApplication1/Data Access Layer/SalesERPDAL.cs
+++ WebApplication1/Data Access Layer/SalesERPDAL.cs
@@ -9,11 +9,12 @@ namespace WebApplication1.Data_Access_Layer
 {
     public class SalesERPDAL :DbContext
     {
+        public DbSet<Employee> Employees { get; set; }
+
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Employee>().ToTable("TblEmployee");
             base.OnModelCreating(modelBuilder);
         }  
     }
-    public Dbset<Employee> Employees { get; set;  }
 }
--- WebApplication1/Models/EmployeeBusinessLayer.cs
+++ WebApplication1/Models/EmployeeBusinessLayer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using WebApplication1.Data_Access_Layer;
 
 namespace WebApplication1.Models
 {
@@ -9,31 +10,13 @@ namespace WebApplication1.Models
     {
         public List<Employee> GetEmployees()
         {
-            List<Employee> employees = new List<Employee>();
-            Employee emp = new Employee();
-            emp.FirstName = "andy";
-            emp.LastName = "qian";
-            emp.Salary = 14000;
-            employees.Add(emp);
 
-            emp = new Employee();
-            emp.FirstName = "qiumegn"
-;
-            emp.LastName = "wan";
-            emp.Salary = 40000;
-            employees.Add(emp);
-
-            emp = new Employee();
-            emp.FirstName = "tingting";
-            emp.LastName = "wan";
-            emp.Salary = 50000;
-            employees.Add(emp);
+            SalesERPDAL salesDal = new SalesERPDAL();
 
 
 
 
-
-                 return employees;
+            return salesDal.Employees.ToList();
         }
     }
 }
